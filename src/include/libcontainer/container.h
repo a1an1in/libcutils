45de55623464392157187ecaf56a0d1e32cd130e
alan <a1an1in@sina.com>
2015-11-09 21:29:33 +0800

    change container funcs into static inline

--- src/include/libcontainer/container.h
+++ src/include/libcontainer/container.h
@@ -121,27 +121,121 @@ void make_pair(pair_t *p,void *key,void *value);
 int destroy_pair(pair_t *p);
 
 container_t *container_creator(uint32_t container_type,allocator_t *allocator,uint8_t lock_type);
-int container_vector_init(container_t *ct,uint32_t data_size,uint32_t capacity);
-int container_list_init(container_t *ct,uint32_t data_size);
-int container_tree_map_init(container_t *ct, uint32_t key_size, uint32_t value_size, key_cmp_fpt key_cmp_func);
-int container_hash_map_init(container_t *ct, uint32_t key_size, uint32_t value_size,uint32_t bucket_size, hash_func_fpt hash_func,key_cmp_fpt key_cmp_func);
-int container_push_front(container_t *ct,void *data);
-int container_push_back(container_t *ct,void *data);
-int container_pop_front(container_t *ct);
-int container_pop_back(container_t *ct);
-int container_insert(container_t *ct, iterator_t it,void *data);
-int container_map_insert(container_t *ct, pair_t *p);
-int container_delete(container_t *ct, iterator_t it);
-int container_destroy(container_t **ct);
-
-iterator_t container_map_search(container_t *ct, void *key);
-iterator_t container_begin(container_t *ct);
-iterator_t container_end(container_t *ct);
-iterator_t iterator_next(iterator_t it);
-iterator_t iterator_prev(iterator_t it);
-int iterator_equal(iterator_t it1,iterator_t it2);
-void* iterator_get_pointer(iterator_t it);
 
 int container_register();
 
+static inline int container_vector_init(container_t *ct,uint32_t data_size,uint32_t capacity)
+{
+	ct->priv.vector_priv.data_size = data_size;
+	ct->priv.vector_priv.capacity  = capacity;
+	dbg_str(DBG_CONTAINER_DETAIL,"container_vector_init");
+
+	return ct->c_ops_p->init(ct);
+}
+static inline int container_list_init(container_t *ct,uint32_t data_size)
+{
+	ct->priv.list_priv.data_size = data_size;
+
+	return ct->c_ops_p->init(ct);
+}
+/*
+ *int container_map_init(container_t *ct,
+ *        uint32_t key_size, uint32_t value_size,
+ *        key_cmp_fpt key_cmp_func)
+ *{
+ *    return ct->c_ops_p->map_init(ct,key_size,value_size,key_cmp_func);
+ *}
+ */
+static inline int container_tree_map_init(container_t *ct,
+		uint32_t key_size, uint32_t value_size,
+		key_cmp_fpt key_cmp_func)
+{
+	ct->priv.rbtree_map_priv.key_size = key_size;
+	ct->priv.rbtree_map_priv.data_size = key_size + value_size;
+	ct->priv.rbtree_map_priv.key_cmp_func = key_cmp_func;
+	return ct->c_ops_p->init(ct);
+}
+static inline int container_hash_map_init(container_t *ct, uint32_t key_size,
+		uint32_t value_size, uint32_t bucket_size,
+		hash_func_fpt hash_func, key_cmp_fpt key_cmp_func)
+{
+	hash_map_priv_t *hpi = &ct->priv.hash_map_priv;
+
+	dbg_str(DBG_CONTAINER_DETAIL,"container_hash_map_init");
+	hpi->key_size    = key_size;
+	hpi->data_size   = key_size + value_size;
+	hpi->hash_func   = hash_func;
+	hpi->key_cmp_fpt = key_cmp_func;
+	hpi->bucket_size = bucket_size;
+	dbg_str(DBG_CONTAINER_DETAIL,"ct->c_ops_p->init(ct)");
+
+	return ct->c_ops_p->init(ct);
+}
+static inline int container_push_front(container_t *ct,void *data)
+{
+	return ct->c_ops_p->push_front(ct,data);
+}
+static inline int container_push_back(container_t *ct,void *data)
+{
+	return ct->c_ops_p->push_back(ct,data);
+}
+static inline int container_pop_front(container_t *ct)
+{
+	return ct->c_ops_p->pop_front(ct);
+}
+static inline int container_pop_back(container_t *ct)
+{
+	return ct->c_ops_p->pop_back(ct);
+}
+static inline iterator_t container_begin(container_t *ct)
+{
+	dbg_str(DBG_CONTAINER_DETAIL,"container_begin");
+	ct->c_ops_p->begin(ct);
+	return ct->c_ops_p->begin(ct);
+}
+static inline iterator_t container_end(container_t *ct)
+{
+	return ct->c_ops_p->end(ct);
+}
+static inline int container_map_insert(container_t *ct, pair_t *p)
+{
+	return ct->c_ops_p->map_insert(ct,p->data);
+}
+static inline int container_insert(container_t *ct,iterator_t it, void *data)
+{
+	return ct->c_ops_p->insert(ct,it,data);
+}
+static inline int container_delete(container_t *ct, iterator_t it)
+{
+	return ct->c_ops_p->del(ct,it);
+}
+static inline iterator_t container_map_search(container_t *ct, void *key)
+{
+	return ct->c_ops_p->map_search(ct,key);
+}
+static inline int container_destroy(container_t **ct)
+{
+	sync_lock_destroy(&(*ct)->head_lock);
+	(*ct)->c_ops_p->destroy(*ct);
+	allocator_mem_free((*ct)->allocator,*ct);
+	*ct = NULL;
+	return 0;
+}
+
+static inline iterator_t iterator_next(iterator_t it)
+{
+	return it.container_p->it_ops_p->next(it);
+}
+static inline iterator_t iterator_prev(iterator_t it)
+{
+	return it.container_p->it_ops_p->prev(it);
+}
+static inline int iterator_equal(iterator_t it1,iterator_t it2)
+{
+	return it1.container_p->it_ops_p->equal(it1,it2);
+}
+static inline void* iterator_get_pointer(iterator_t it)
+{
+	return it.container_p->it_ops_p->iterator_get_pointer(it);
+}
 #endif
