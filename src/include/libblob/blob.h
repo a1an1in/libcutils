1929a4a1916e1201ba1fb3f3241e58e74f140e5b
a1an1in <a1an1in@sina.com>
2016-11-09 17:36:25 +0800

    add lic to libblob

--- src/include/libblob/blob.h
+++ src/include/libblob/blob.h
@@ -20,9 +20,6 @@ typedef struct blob_attr_s{
     uint16_t len;
     uint16_t name_len;
     uint8_t value[0];
-/*
- *} blob_attr_t;
- */
 }__attribute__((packed)) blob_attr_t;
 
 enum msgblob_type {       
@@ -49,9 +46,7 @@ int blob_add(blob_t *blob, uint8_t type, char *name, void *value, uint16_t value
 int blob_add_u8(blob_t *blob, char *name, uint8_t val);
 int blob_add_u16(blob_t *blob, char *name, uint16_t val);
 int blob_add_u32(blob_t *blob, char *name, uint32_t val);
-/*
- *int blob_add_u64(blob_t *blob, char *name, uint64_t val);
- */
+int blob_add_u64(blob_t *blob, char *name, uint64_t val);
 int blob_add_string(blob_t *blob, char *name, char *str);
 int blob_add_table_start(blob_t *blob, char *name);
 int blob_add_table_end(blob_t *blob);
--- src/libblob/blob.c
+++ src/libblob/blob.c
@@ -1,3 +1,34 @@
+/**
+ * @file blob.c
+ * @synopsis 
+ * @author alan(a1an1in@sina.com)
+ * @version 1
+ * @date 2016-11-09
+ */
+/* Copyright (c) 2015-2020 alan lin <a1an1in@sina.com>
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
 #include <stdio.h>
 #include <libblob/utils.h>
 #include <libdbg/debug.h>
@@ -226,21 +257,6 @@ int blob_parse_to_attr(const struct blob_policy_s *policy,
             }
         }
     }
-
-#if 0
-    int i;
-    uint8_t *pos;
-    uint8_t *end = (uint8_t *)data + len;
-
-    for(pos = (uint8_t *)data; pos < end; pos = (uint8_t *)blob_next((blob_attr_t *)pos)) {
-        for(i = 0; i < policy_count; i++) {
-            if(!strncmp(blob_get_name((blob_attr_t *)pos), policy[i].name,strlen(policy[i].name))) {
-                tb[i] = (blob_attr_t *)pos;
-                dbg_str(DBG_DETAIL,"found policy %d,name=%s",  i, policy[i].name);
-            }
-        }
-    }
-#endif
 
     return 0;
 }
