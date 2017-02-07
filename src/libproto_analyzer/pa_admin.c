a6c273ea42f44fc7e3cd1a5c383caddd8d928447
a1an1in <a1an1in@sina.com>
2017-02-07 08:54:37 -0500

    [modulate_code_format:libproto_analyzer] replace tab with 4 blank space

--- src/libproto_analyzer/pa_admin.c
+++ src/libproto_analyzer/pa_admin.c
@@ -55,55 +55,55 @@ pa_admin_t *pa_admin;
 pa_admin_t *pa_admin_create(allocator_t *allocator,
                             uint8_t key_size,uint8_t bucket_size)
 {
-	pa_admin_t *admin = NULL;
-	hash_map_t *hmap = NULL;
-	uint32_t value_size;
-	pair_t *pair;
+    pa_admin_t *admin = NULL;
+    hash_map_t *hmap = NULL;
+    uint32_t value_size;
+    pair_t *pair;
 
-	admin = (pa_admin_t *)allocator_mem_alloc(allocator,sizeof(pa_admin_t));
-	if(admin == NULL){
-		dbg_str(DBG_ERROR,"allocator_mem_alloc");
-		goto err_alloc_admin;
-	}
+    admin = (pa_admin_t *)allocator_mem_alloc(allocator,sizeof(pa_admin_t));
+    if(admin == NULL){
+        dbg_str(DBG_ERROR,"allocator_mem_alloc");
+        goto err_alloc_admin;
+    }
     memset(admin,0,sizeof(pa_admin_t));
 
-	admin->allocator = allocator;
+    admin->allocator = allocator;
 
-	hmap = hash_map_alloc(allocator);
-	if(hmap == NULL){
-		dbg_str(DBG_ERROR,"hash_map_create");
-		goto err_create_hash_map;
-	}
-	value_size = sizeof(struct protocol_analyzer_s *);
-	hash_map_init(hmap, key_size,//uint32_t key_size,
-			      value_size ,//uint32_t value_size
-			      bucket_size);
-	admin->hash_map = hmap;
+    hmap = hash_map_alloc(allocator);
+    if(hmap == NULL){
+        dbg_str(DBG_ERROR,"hash_map_create");
+        goto err_create_hash_map;
+    }
+    value_size = sizeof(struct protocol_analyzer_s *);
+    hash_map_init(hmap, key_size,//uint32_t key_size,
+                  value_size ,//uint32_t value_size
+                  bucket_size);
+    admin->hash_map = hmap;
 
-	pair = create_pair(key_size,value_size);
-	if(pair == NULL){
-		dbg_str(DBG_ERROR,"hash_map_create");
-		goto err_create_pair;
-	}
-	admin->pair = pair;
-	return admin;
+    pair = create_pair(key_size,value_size);
+    if(pair == NULL){
+        dbg_str(DBG_ERROR,"hash_map_create");
+        goto err_create_pair;
+    }
+    admin->pair = pair;
+    return admin;
 
 err_create_pair:
-	hash_map_destroy(admin->hash_map);
+    hash_map_destroy(admin->hash_map);
 err_create_hash_map:
-	allocator_mem_free(allocator,admin);
+    allocator_mem_free(allocator,admin);
 err_alloc_admin:
-	return NULL;
+    return NULL;
 }
 
 void pa_admin_destroy(pa_admin_t *admin)
 {
-	allocator_t *allocator = admin->allocator;;
+    allocator_t *allocator = admin->allocator;;
 
-	dbg_str(DBG_VIP,"pa_admin_destroy");
-	destroy_pair(admin->pair);
-	hash_map_destroy(admin->hash_map);
-	allocator_mem_free(allocator,admin);
+    dbg_str(DBG_VIP,"pa_admin_destroy");
+    destroy_pair(admin->pair);
+    hash_map_destroy(admin->hash_map);
+    allocator_mem_free(allocator,admin);
 }
 /*
  *向 pa_admin注册协议解析器
@@ -120,11 +120,11 @@ void pa_admin_add_protocol_analyzer(pa_admin_t *admin,void *key,struct protocol_
         addr_buffer[i] = (addr >> ((sizeof(pa) - 1 - i) * 8)) & 0xff;
     }
 
-	/*
-	 *dbg_str(DBG_DETAIL,"pa_admin_add_protocol_analyzer,key=%s,pa addr=%p",key,pa);
-	 */
-	make_pair(admin->pair,key,(void *)addr_buffer);
-	hash_map_insert_data(admin->hash_map,admin->pair->data);
+    /*
+     *dbg_str(DBG_DETAIL,"pa_admin_add_protocol_analyzer,key=%s,pa addr=%p",key,pa);
+     */
+    make_pair(admin->pair,key,(void *)addr_buffer);
+    hash_map_insert_data(admin->hash_map,admin->pair->data);
 }
 /*
  *通过key，索引想用的解析器
@@ -132,9 +132,9 @@ void pa_admin_add_protocol_analyzer(pa_admin_t *admin,void *key,struct protocol_
 struct protocol_analyzer_s *
 pa_admin_get_protocol_analyzer(pa_admin_t *admin ,void *key)
 {
-	hash_map_pos_t pos;
-	uint8_t *addr_p;
-	struct protocol_analyzer_s *pa;
+    hash_map_pos_t pos;
+    uint8_t *addr_p;
+    struct protocol_analyzer_s *pa;
     uint64_t addr = 0;
     int ret;
     uint8_t i;
@@ -142,9 +142,9 @@ pa_admin_get_protocol_analyzer(pa_admin_t *admin ,void *key)
     ret = hash_map_search(admin->hash_map, key,&pos);
     if(ret < 0) return NULL;
 
-	if(pos.hlist_node_p == NULL){
-		dbg_str(DBG_WARNNING,"not find relevent pa");
-		pa = NULL;
+    if(pos.hlist_node_p == NULL){
+        dbg_str(DBG_WARNNING,"not find relevent pa");
+        pa = NULL;
     }else{
         addr_p = (uint8_t *)hash_map_pos_get_pointer(&pos);
 
@@ -154,60 +154,60 @@ pa_admin_get_protocol_analyzer(pa_admin_t *admin ,void *key)
         pa = (struct protocol_analyzer_s *)addr;
 
         pa_reset_vlen_flag(pa);
-	}
-	return pa;
+    }
+    return pa;
 }
 
 void pa_admin_generate_key(int proto_num,char *key)
 {
-	sprintf(key,"pa%x",proto_num);
+    sprintf(key,"pa%x",proto_num);
 }
 void pa_admin_register_protocol_analyzers(pa_admin_t *admin,
                                           protocol_format_set_t *pfs)
 {
-	struct protocol_analyzer_s *pa;
-	allocator_t *allocator = admin->allocator;;
-	struct list_head **hl_head_p2 = pfs->list_head_p2;
-	uint32_t protocol_num = pfs->proto_max_num;
-	uint8_t i = 0;
+    struct protocol_analyzer_s *pa;
+    allocator_t *allocator = admin->allocator;;
+    struct list_head **hl_head_p2 = pfs->list_head_p2;
+    uint32_t protocol_num = pfs->proto_max_num;
+    uint8_t i = 0;
 #define MAX_KEY_LEN 10
-	char key[MAX_KEY_LEN];
+    char key[MAX_KEY_LEN];
 #undef MAX_KEY_LEN
 
-	admin->pfs = pfs;
+    admin->pfs = pfs;
 
-	if(!hl_head_p2){
-		dbg_str(DBG_ERROR,"hl_head is NULL");
-		return;
-	}
-	if(protocol_num > 100){
-		dbg_str(DBG_ERROR,"proto num err");
-	}
-	/*
-	 *把协议格式集中注册的协议格式，在pa_admin注册个协议分析器，
-	 *这样在需要前就提前生成，提高代码性能
-	 * */
-	for(i = 0; i < protocol_num; i++){
-		if(hl_head_p2[i] != NULL){
-			pa = pa_create_protocol_analyzer(allocator);
-			pa_init_protocol_analyzer(pfs->proto_base_addr + i, pfs, pa);
-			pa_admin_generate_key(pfs->proto_base_addr + i,key);
-			pa_admin_add_protocol_analyzer(admin,key,pa);
-		}
-	}
+    if(!hl_head_p2){
+        dbg_str(DBG_ERROR,"hl_head is NULL");
+        return;
+    }
+    if(protocol_num > 100){
+        dbg_str(DBG_ERROR,"proto num err");
+    }
+    /*
+     *把协议格式集中注册的协议格式，在pa_admin注册个协议分析器，
+     *这样在需要前就提前生成，提高代码性能
+     * */
+    for(i = 0; i < protocol_num; i++){
+        if(hl_head_p2[i] != NULL){
+            pa = pa_create_protocol_analyzer(allocator);
+            pa_init_protocol_analyzer(pfs->proto_base_addr + i, pfs, pa);
+            pa_admin_generate_key(pfs->proto_base_addr + i,key);
+            pa_admin_add_protocol_analyzer(admin,key,pa);
+        }
+    }
 }
 
 void test_pa_admin()
 {
-	protocol_format_set_t *pfs_p;
-	uint32_t ret = 0;
-	allocator_t *allocator = allocator_get_default_alloc();
+    protocol_format_set_t *pfs_p;
+    uint32_t ret = 0;
+    allocator_t *allocator = allocator_get_default_alloc();
     pa_admin_t *pa_admin;
     struct protocol_analyzer_s *pa;
 
-	pfs_p = pfs_create_proto_format_set(allocator);
-	init_proto_format_set(0x3000,100,pfs_p);
-	pfs_set_pdt_drv_proto_format(pfs_p);
+    pfs_p = pfs_create_proto_format_set(allocator);
+    init_proto_format_set(0x3000,100,pfs_p);
+    pfs_set_pdt_drv_proto_format(pfs_p);
 
     pa_admin = pa_admin_create(allocator,7, 40);
 
@@ -220,8 +220,8 @@ void test_pa_admin()
         dbg_str(DBG_DETAIL,"not find pa");
     }
     /*
-	 *dbg_str(DBG_DETAIL,"pfs_destroy_protocol_format_set");
-	 *pfs_destroy_protocol_format_set(pfs_p);
+     *dbg_str(DBG_DETAIL,"pfs_destroy_protocol_format_set");
+     *pfs_destroy_protocol_format_set(pfs_p);
      */
 
 }
