8c136242f21066964cf12039ddf032c6232bb96c
a1an1in <alanlin@cloudtropy.com>
2017-08-29 18:48:50 +0800

    [update_func:N/A] solve compiling warnnigs

--- src/include/test.h
+++ src/include/test.h
@@ -43,6 +43,7 @@ extern int test_array_stack();
 extern int test_miscellany_net();
 extern int lab();
 extern int lab2();
+extern int lab6(int argc,char **argv);
 extern void args_print_help_test_info(args_processor_t *p);
 extern int test_json(void);
 extern int test_inet_wireless(int argc, char **argv);
--- src/liballoc/test_ctr_alloc.c
+++ src/liballoc/test_ctr_alloc.c
@@ -44,6 +44,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <liballoc/allocator.h>
 #include <libdbg/debug.h>
 #include "cutils_re.h"
